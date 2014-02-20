5abb8fc2b848b821e614c093cae70e0f841f7223
Kevin Yeh <kevinyeah@utexas.edu>
2014-02-19 21:25:15 -0600

    added common packages setup script

--- utils/README.md
+++ utils/README.md
@@ -2,7 +2,3 @@ utils
 =====
 
 Various scripts for good times.
-
-usrstartup.sh: start redshift from Austin, TX and pulseaudio.
-
-musicmanagersync.pl: Start Google Music Manager and restart when it runs into memory problems.
---
+++ utils/setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get install pip git subversion emacs-24.3 redshift imagemagick ruby rubygems perl node libglu1-mesa-dev freeglut3-dev mesa-common-dev libglew-dev libpng-dev xclip haskell-platform dropbox #multi-pipelight rails
+sudo pip howdoi
+sudo gem install bropages
+sudo npm -g install jade html2jade
+
+# Android Studio / GenyMotion
+# intellij
+# CCCP or Similar Codec Pack
+# gibo
+# jdk (1.7+)
