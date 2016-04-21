e17b5c2b6fe46f03514269c427b5da941347fcd7
Kevin Yeh <kevinyeah@utexas.edu>
2016-04-21 13:22:28 -0500

    Update setup

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install pip git subversion emacs-24.3 redshift imagemagick ruby rubygems perl node libglu1-mesa-dev freeglut3-dev mesa-common-dev libglew-dev libpng-dev xclip haskell-platform dropbox #multi-pipelight rails
+sudo apt-get install pip git subversion emacs-24.3 redshift imagemagick ruby rubygems perl node libglu1-mesa-dev freeglut3-dev mesa-common-dev libglew-dev libpng-dev xclip haskell-platform dropbox autojump #multi-pipelight rails
 sudo pip howdoi
 sudo gem install bropages
 sudo npm -g install jade html2jade
@@ -8,3 +8,5 @@ sudo npm -g install jade html2jade
 # CCCP or Similar Codec Pack
 # gibo
 # jdk (1.7+)
+# rust
+# golang
